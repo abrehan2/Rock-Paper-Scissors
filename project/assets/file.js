bfabe1b2db1ab26879df193fdf6c4bee5154e13e
Abdul Rehan <100872683+abrehan2@users.noreply.github.com>
2023-01-27 21:36:06 +0500

    first commit

--- project/assets/file.js
+++ project/assets/file.js
@@ -1,18 +1,13 @@
 let comp_choice = document.querySelector("#computer-choice");
-
 let user_choice = document.querySelector("#user-choice");
-
 let result = document.querySelector("#result");
-
 let possible_choices = document.querySelectorAll('button');
-
 let user_select;
 
 possible_choices.forEach(choice => {
-    choice.addEventListener("click", e => {
-        user_select = e.target.id;
+  choice.addEventListener("click", e => {
+  user_select = e.target.id;
   user_choice.innerHTML = user_select;
-
   generate_computer_choice();
   get_result();
     });
@@ -22,17 +17,14 @@ function generate_computer_choice()
 {
     var arr = ['rock', 'paper', 'scissors'];
     var random_num = Number.parseInt((Math.random() * 3));
-
     comp_choice.innerHTML = arr[random_num];   
 }
 
-function get_result()
-{
+function get_result(){
 
     var comp_result = comp_choice.innerHTML;
 
 // CASE: 1
-
     if(user_select == 'rock' && comp_result == 'rock')
     {
         result.innerHTML = `Draw`;
@@ -48,9 +40,7 @@ else if (user_select == 'rock' && comp_result == 'paper')
     result.innerHTML = `You Lose!`;
 }
 
-
 // CASE: 2
-
 if(user_select == 'paper' && comp_result == 'paper')
 {
     result.innerHTML = `Draw`;
@@ -67,7 +57,6 @@ result.innerHTML = `You Lose!`;
 }
 
 // CASE: 3
-
 if(user_select == 'scissors' && comp_result == 'scissors')
 {
     result.innerHTML = `Draw`;
